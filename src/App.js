a8372564991f87d0093a68bca757d55164f2513d
avneet6 <143005707+avneet6@users.noreply.github.com>
2024-11-30 01:06:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+ import Register from './components/Register';
+import List from './components/List';
+
+
+
+
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState(''); 
+  const handleSearch = (term) => { setSearchTerm(term); };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    
+<Router>
+       <div className="App">
+        <Navbar onSearch={handleSearch}/>
+        
+         <Routes> 
+          
+        <Route path='/register' element={<Register />} />
+           <Route path='/list' element={<List searchTerm={searchTerm} /> } />
+           </Routes>
+           <Footer/>
+            </div> 
+            </Router>
+            
   );
 }
 
